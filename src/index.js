65a0188c55b79f656d7634778643fe1f4e7647e9
RuslanGon <ruslan441006@gmail.com>
2024-09-23 16:08:59 +0300

    wer

--- src/index.js
+++ src/index.js
@@ -18,6 +18,10 @@ app.get("/", (req, res, next) => {
   res.send("Hello Ruslan");
 });
 
+app.get('/error', (req, res, next) => {
+    next(new Error('some error here'));
+});
+
 app.use((req, res, next) => {
 res.status(404).send('Oops, route was not found');
 });
