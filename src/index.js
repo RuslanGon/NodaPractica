0a143d5cf0f04fe3b1be850faff29077ea1b1726
RuslanGon <ruslan441006@gmail.com>
2024-09-23 15:44:54 +0300

    wer

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
-import express from 'express';
-import pino from 'pino-http';
+import express from "express";
+import pino from "pino-http";
+import cors from "cors";
 
 const app = express();
 
 app.use(
-    pino({
-      transport: {
-        target: 'pino-pretty', 
-      },
-    })
-  );
+  pino({
+    transport: {
+      target: "pino-pretty",
+    },
+  })
+);
 
-app.get('/', (req, res, next) => {
-res.send('Hello Ruslan');
+app.use(cors({
+    origin:
+}));
+
+app.get("/", (req, res, next) => {
+  res.send("Hello Ruslan");
 });
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
