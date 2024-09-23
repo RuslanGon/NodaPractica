a213fa353e1df9947fc3f716f78f5229d04483f3
RuslanGon <ruslan441006@gmail.com>
2024-09-23 15:02:35 +0300

    wer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-const message = 'Hello world';
+import express from 'express';
 
-console.log(message);
+const app = express();
+
+
+app.listen(3000,() => {
+console.log('Server is running on port 3000');
+});
