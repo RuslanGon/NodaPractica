25b054909bb7f2694892d8c7dfe56fdaaa6618cd
RuslanGon <ruslan441006@gmail.com>
2024-09-24 16:16:02 +0300

    wer

---
+++ src/middlewars/models/student.js
@@ -0,0 +1,10 @@
+import { Schema } from "mongoose";
+
+export const studentSchema = new Schema({
+    name: {type: String, required: true },
+    age: {type: Number, required: true },
+    gender: {type: String, required: true, enum: ['male', 'famale', 'other'] },
+    avgMark: {type: Number, required: true, min: 1, max:17 },
+    onDuty: {type: Boolean, default: false },
+});
+
