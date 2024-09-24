f9b1a22ee9facb93763e759b86121ddcaa73e8a5
RuslanGon <ruslan441006@gmail.com>
2024-09-24 17:16:51 +0300

    wer

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
-import express from "express";
-import pino from "pino-http";
-import cors from "cors";
-import { env } from "./utils/env.js";
-import { ENV_VARS } from "./constants/index.js";
-import { notFoundMiddleware } from "./middlewars/notFoundMiddleware.js";
-import { errorHandlerMiddleware } from "./middlewars/errorHandlerMiddleware.js";
-
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { env } from './utils/env.js';
+import { ENV_VARS } from './constants/index.js';
+import { notFoundMiddleware } from './middlewars/notFoundMiddleware.js';
+import { errorHandlerMiddleware } from './middlewars/errorHandlerMiddleware.js';
+import { getAllStudents, getStudentById } from './services/students.js';
 
 export const startServer = () => {
   const app = express();
@@ -13,19 +13,38 @@ export const startServer = () => {
   app.use(
     pino({
       transport: {
-        target: "pino-pretty",
+        target: 'pino-pretty',
       },
-    })
+    }),
   );
 
   app.use(cors());
 
-  app.get("/", (req, res, next) => {
-    res.send("Hello Ruslan");
+  app.get('/students', async (req, res, next) => {
+    const students = await getAllStudents();
+    res.json({
+      status: 200,
+      message: 'successfully get all students',
+      data: students
+    });
   });
 
-  app.get("/error", (req, res, next) => {
-    next(new Error("some error here"));
+  app.get('/students/:studentId', async (req, res, next) => {
+    const id = req.params.studentId;
+    const student = await getStudentById(id);
+    res.json({
+      status: 200,
+      message: `successfully get student by ${id}`,
+      data: student,
+    });
+  });
+
+  app.get('/', (req, res, next) => {
+    res.send('Hello Ruslan');
+  });
+
+  app.get('/error', (req, res, next) => {
+    next(new Error('some error here'));
   });
 
   app.use(notFoundMiddleware);
@@ -36,5 +55,4 @@ export const startServer = () => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${3000}`);
   });
-
 };
