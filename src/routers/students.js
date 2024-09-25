112cbaddaf197cda1227b61692c78b9f1826dc15
RuslanGon <ruslan441006@gmail.com>
2024-09-25 12:21:22 +0300

    wer

--- src/routers/students.js
+++ src/routers/students.js
@@ -1,33 +1,10 @@
 import { Router } from "express";
-import { getAllStudents, getStudentById } from "../services/students.js";
+import { getStudentByIdController, getStudentsController } from "../controllers/students.js";
 
 const studentsRouter = Router();
 
-studentsRouter.get('/students', async (req, res, next) => {
-    const students = await getAllStudents();
-    res.json({
-      status: 200,
-      message: 'successfully get all students',
-      data: students
-    });
-  });
+studentsRouter.get('/students', getStudentsController );
 
-  studentsRouter.get('/students/:studentId', async (req, res, next) => {
-    const id = req.params.studentId;
-    const student = await getStudentById(id);
-
-    if (!student) {
-      return res.status(404).json({
-        status: 404,
-        message: `Student with ID ${id} not found`,
-      });
-    }
-
-    res.json({
-      status: 200,
-      message: `successfully get student by ${id}`,
-      data: student,
-    });
-  });
+  studentsRouter.get('/students/:studentId', getStudentByIdController );
 
   export default studentsRouter;
