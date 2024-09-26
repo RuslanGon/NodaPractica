edb070ab06de6d75259768f8d1933fa64315423a
RuslanGon <ruslan441006@gmail.com>
2024-09-26 17:02:05 +0300

    wer

--- src/routers/students.js
+++ src/routers/students.js
@@ -1,20 +1,21 @@
 import { Router } from "express";
 import { cteateStudentController, deleteStudentByIdController, getStudentByIdController, getStudentsController, patchStudentController, putStudentController } from "../controllers/students.js";
 import { ctrlWrapper } from "../middlewars/ctrlWrapper.js";
+import { validateMongoId } from "../middlewars/validateMongoId.js";
 
 const studentsRouter = Router();
 
 studentsRouter.get('/students', ctrlWrapper(getStudentsController));
 
-studentsRouter.get('/students/:studentId', ctrlWrapper(getStudentByIdController));
+studentsRouter.get('/students/:studentId',validateMongoId, ctrlWrapper(getStudentByIdController));
 
 studentsRouter.post('/students', ctrlWrapper(cteateStudentController));
 
-studentsRouter.patch('/students/:studentId', ctrlWrapper(patchStudentController));
+studentsRouter.patch('/students/:studentId', validateMongoId, ctrlWrapper(patchStudentController));
 
-studentsRouter.put('/students/:studentId', ctrlWrapper(putStudentController));
+studentsRouter.put('/students/:studentId',validateMongoId, ctrlWrapper(putStudentController));
 
-studentsRouter.delete('/students/:studentId', ctrlWrapper(deleteStudentByIdController));
+studentsRouter.delete('/students/:studentId', validateMongoId, ctrlWrapper(deleteStudentByIdController));
 
 
 
