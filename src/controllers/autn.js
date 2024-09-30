dcafbb14ce55ec4f311189f3009945bf23cd8e1b
RuslanGon <ruslan441006@gmail.com>
2024-09-30 15:42:54 +0300

    wer

--- src/controllers/autn.js
+++ src/controllers/autn.js
@@ -1,3 +1,10 @@
-export const registorController = (req, res, next) => {
+import { createUser } from "../services/auth.js";
 
+export const registorController = async (req, res, next) => {
+const user = await createUser(req.body);
+res.json({
+status: 200,
+message: 'User is created',
+data: {user}
+});
 };
