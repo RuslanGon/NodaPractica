5946fd8841642106caa65473d7917ecb76773fca
RuslanGon <ruslan441006@gmail.com>
2024-09-30 15:51:25 +0300

    wer

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,5 +1,7 @@
 import { User } from "../db/models/user.js";
+import bcrypt from 'bcrypt';
 
 export const createUser = async (payload) => {
-return await User.create(payload);
+const hashPassword = await bcrypt.hash(payload.password, 10);
+return await User.create({...payload, password: hashPassword});
 };
