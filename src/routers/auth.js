4806458df4b7bcdbd64e077fdb12636aeaa5202c
RuslanGon <ruslan441006@gmail.com>
2024-09-30 15:28:31 +0300

    wer

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import { ctrlWrapper } from '../middlewars/ctrlWrapper.js';
+import { registorController } from '../controllers/autn.js';
+import { validateBody } from '../middlewars/validateBody.js';
+import { registorSchema } from '../validation/registorSchema.js';
 
 const authRouter = Router();
 
-authRouter.post('/registor');
+authRouter.post('/registor', validateBody(registorSchema), ctrlWrapper(registorController));
 authRouter.post('/login');
 authRouter.post('/refresh-token');
 authRouter.post('/logout');
