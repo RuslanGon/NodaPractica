f0ea264aae7181adbe2e9ad4f524e02e6de7e8f0
RuslanGon <ruslan441006@gmail.com>
2024-10-01 18:52:00 +0300

    wer

--- src/services/auth.js
+++ src/services/auth.js
@@ -4,36 +4,36 @@ import bcrypt from 'bcrypt';
 import crypto from 'crypto';
 import { Session } from "../db/models/session.js";
 
+const createSession = () => {
+  return {
+    accessToken: crypto.randomBytes(20).toString('base64'),
+    refreshToken: crypto.randomBytes(20).toString('base64'),
+    accessTokenValidUntil: new Date(Date.now() + 15 * 60 * 1000),
+    refreshTokenValidUntil: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000)
+  };
+};
+
 
 export const createUser = async (payload) => {
 const hashPassword = await bcrypt.hash(payload.password, 10);
 return await User.create({...payload, password: hashPassword});
 };
 
-export const loginUser = async ({email, password}) => {
-const user = await User.findOne({email});
-if(!user){
-throw createHttpError(404, 'User not found');
-}
+export const loginUser = async ({ email, password }) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
 
-const areEqual = await bcrypt.compare(password, user.password);
-if(!areEqual){
+  const areEqual = await bcrypt.compare(password, user.password);
+  if (!areEqual) {
     throw createHttpError(401, 'Unathorize');
-}
-
-await Session.deleteOne({ userId: user._id });
-
-const accessToken = crypto.randomBytes(20).toString('base64');
-const refreshToken = crypto.randomBytes(20).toString('base64');
-
-return await Session.create({
-    accessToken,
-    refreshToken,
-    userId:user._id,
-    accessTokenValidUntil: new Date(Date.now() + 15 * 60 * 1000),
-    refreshTokenValidUntil: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000)
-});
-
+  }
+  await Session.deleteOne({ userId: user._id });
+  return await Session.create({
+    userId: user._id,
+    ...createSession(),
+  });
 };
 
 export const logoutUser = async ({ sessionId, sessionToken }) => {
@@ -45,5 +45,23 @@ export const logoutUser = async ({ sessionId, sessionToken }) => {
 
 
 export const refreshUser =  async ({ sessionId, sessionToken }) => {
+const session = await Session.findOne({_id: sessionId, refreshToken: sessionToken});
 
+if(!session){
+  throw createHttpError(401, 'Session not found');
+}
+
+if(new Date() > session.refreshTokenValidUntil){
+  throw createHttpError(401, 'Session is expired');
+}
+
+const user = await User.findById(session.userId);
+if (!user) {
+  throw createHttpError(401, 'Session not found');
+}
+
+return await Session.create({
+  userId: user._id,
+  ...createSession(),
+});
 };
